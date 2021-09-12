9c9e6057acdcfb0e0e9a411d0581c58120629b53
felipe-brando <f.brandotalero@gmail.com>
2021-09-12 16:20:46 +0200

    supression d'espaces

--- app.js
+++ app.js
@@ -14,24 +14,21 @@ let app = {
 
  },
 
- handleTitleClick: function (event) {
+  handleTitleClick: function (event) {
    
-  event.preventDefault();
-  const currentClick = event.currentTarget;
-  //console.log(currentClick)
-  const arrows = document.querySelectorAll('.accordion__arrow');
-  for (let arrowIndex = 0 ; arrowIndex < arrows.length; arrowIndex++) {
-    arrows[arrowIndex].parentNode.classList.remove('open');
-    arrows[arrowIndex].classList.remove('arrow-open');
+    event.preventDefault();
+    const currentClick = event.currentTarget;
+    //console.log(currentClick)
+    const arrows = document.querySelectorAll('.accordion__arrow');
+    for (let arrowIndex = 0 ; arrowIndex < arrows.length; arrowIndex++) {
+      arrows[arrowIndex].parentNode.classList.remove('open');
+      arrows[arrowIndex].classList.remove('arrow-open');
+    }
+    currentClick.classList.add('open');
+    const currentArrow = currentClick.querySelector('.accordion__arrow');
+    currentArrow.classList.add('arrow-open');
   }
-  currentClick.classList.add('open');
-  const currentArrow = currentClick.querySelector('.accordion__arrow');
-  currentArrow.classList.add('arrow-open');
-
   
-}
-  
-
 }
 
 document.addEventListener('DOMContentLoaded', app.init);
